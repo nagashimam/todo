582738ec7f436872c9289ee0e716784ea58f6d3b
masato <masato.ngsm@gmail.com>
2021-12-19 21:08:32 +0900

    変更が検知できなくなったのでOnPushを外す

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoListComponent {
   /**
